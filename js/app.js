f50659c5994ce3941ec94734eee499cb041e18e4
alfonsovillanueva <alfonso@villanuevagarcia.com>
2022-05-10 20:52:17 -0500

    Proyecto Finalizado

--- js/app.js
+++ js/app.js
@@ -1,18 +1,44 @@
 const modalImagen = document.querySelector("#modal-imagen");
 
-modalImagen.addEventListener("show.bs.modal", function (e) {
-	const enlace = e.relatedTarget;
-	const rutaImagen = enlace.getAttribute("data-alfgow");
-	const imagen = document.createElement("IMG");
-	imagen.src = `./img/${rutaImagen}.jpg`;
-	imagen.classList.add("img-fluid");
-	imagen.alt = "Imagen Galeria";
+if (modalImagen) {
+	modalImagen.addEventListener("show.bs.modal", function (e) {
+		const enlace = e.relatedTarget;
+		const rutaImagen = enlace.getAttribute("data-alfgow");
+		const imagen = document.createElement("IMG");
+		imagen.src = `./img/${rutaImagen}.jpg`;
+		imagen.classList.add("img-fluid");
+		imagen.alt = "Imagen Galeria";
 
-	const mostarImg = document.querySelector(".modal-body");
+		const mostarImg = document.querySelector(".modal-body");
 
-	while (mostarImg.firstChild) {
-		mostarImg.removeChild(mostarImg.firstChild);
-	}
+		while (mostarImg.firstChild) {
+			mostarImg.removeChild(mostarImg.firstChild);
+		}
 
-	mostarImg.appendChild(imagen);
-});
+		mostarImg.appendChild(imagen);
+	});
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	"use strict";
+
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	var forms = document.querySelectorAll(".needs-validation");
+
+	// Loop over them and prevent submission
+	Array.prototype.slice.call(forms).forEach(function (form) {
+		form.addEventListener(
+			"submit",
+			function (event) {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+
+				form.classList.add("was-validated");
+			},
+			false
+		);
+	});
+})();
